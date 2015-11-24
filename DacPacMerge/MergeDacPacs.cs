209245a6c207e8587857a8319ec53254a3556f96
Ruban Siva <sivaru@MLP04736.uk.mizuho-sc.com>
2015-11-24 15:03:02 +0000

    add

---
+++ DacPacMerge/MergeDacPacs.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.SqlServer.Dac;
+using Microsoft.SqlServer.Dac.Model;
+using Public.Dac.Samples;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DacPacMergeExample
+{
+    internal class MergeDacPacs
+    {
+        internal static void Merge()
+        {
+            var samplePackagagePath = @"..\..\Temp\Sample.dacpac";
+            TSqlModel sampleModel = new TSqlModel(samplePackagagePath, DacSchemaModelStorageType.Memory);
+
+
+            var targetPackagePath = @"merged.dacpac";
+
+            DisposableList disposables = new DisposableList();
+
+            DacPackageExtensions.BuildPackage(
+                    targetPackagePath,
+                    sampleModel,
+                    new PackageMetadata(), // Describes the dacpac. 
+                    new PackageOptions());  // Use this to specify the deployment contributors, refactor log to include in package
+
+            //DacPackage package = disposables.Add(DacPackage.Load(targetPackagePath, DacSchemaModelStorageType.Memory, FileAccess.ReadWrite));
+            //package.UpdateModel(cilModel, new PackageMetadata());
+        }
+
+    }
+}
