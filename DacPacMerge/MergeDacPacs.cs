e74096f06c9c4b31384134c6e1969042f512bcf1
Ed Elliott <goeddieuk@gmail.com>
2015-11-25 09:08:00 +0000

    Demo to add individual scripts to a new model

--- DacPacMerge/MergeDacPacs.cs
+++ DacPacMerge/MergeDacPacs.cs
@@ -14,17 +14,26 @@ namespace DacPacMergeExample
     {
         internal static void Merge()
         {
-            var samplePackagagePath = @"..\..\Temp\Sample.dacpac";
+            var samplePackagagePath = @"C:\Dev\ed\DacMergeExample\SampleSQLProj\bin\Debug\Sample.dacpac";
             TSqlModel sampleModel = new TSqlModel(samplePackagagePath, DacSchemaModelStorageType.Memory);
-
 
             var targetPackagePath = @"merged.dacpac";
 
             DisposableList disposables = new DisposableList();
 
+            var newModel = new TSqlModel(sampleModel.Version, sampleModel.CopyModelOptions());
+            foreach (var item in sampleModel.GetObjects(DacQueryScopes.UserDefined))
+            {
+                string script;
+                if (item.TryGetScript(out script))
+                {
+                    newModel.AddObjects(script);
+                }
+            }
+
             DacPackageExtensions.BuildPackage(
                     targetPackagePath,
-                    sampleModel,
+                    newModel,
                     new PackageMetadata(), // Describes the dacpac. 
                     new PackageOptions());  // Use this to specify the deployment contributors, refactor log to include in package
 
